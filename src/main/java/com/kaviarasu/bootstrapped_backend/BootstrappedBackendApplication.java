e439f8ecda813de8b9c7a88e9368e05ac7793a04
kaviarasuns <kaviarasu_ns@icloud.com>
2025-02-04 18:17:24 +0530

    Q41 - Integrating Protected APIs - Real-Time Amazon Data (Product Search and Products By Category)

---
+++ src/main/java/com/kaviarasu/bootstrapped_backend/BootstrappedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kaviarasu.bootstrapped_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrappedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrappedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaviarasu/bootstrapped_backend/BootstrappedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaviarasu.bootstrapped_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrappedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
